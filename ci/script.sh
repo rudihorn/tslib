b32995dfbaf1ebc8b8a7c4b78aa65678e413d405
Rudi Horn <dyn-git@rudi-horn.de>
2018-05-22 11:42:28 +0100

    added travis CI

---
+++ ci/script.sh
@@ -0,0 +1,17 @@
+set -euxo pipefail
+
+main() {
+    cargo check --target $TARGET
+
+    if [ "$TARGET" = "thumbv7m-none-eabi" ]; then
+        # cargo check --example stm32 --target $TARGET
+    fi
+
+    if [ "$TARGET" = "x86_64-unknown-linux-gnu" ]; then
+        # the --tests is required to ignore the examples
+        # which will not compile under x86
+        cargo test --target $TARGET --tests
+    fi
+}
+
+main
